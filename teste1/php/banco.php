bd25346d01e8af46afcf1407949432560930c2ab
Augusto Poloni <a_poloni@unoesc.edu.br>
2024-12-04 22:36:15 -0300

    Delete teste1 directory

--- teste1/php/banco.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $con = new PDO("mysql:host=localhost;dbname=banco1;charset=utf8", "root", "");
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erro ao conectar ao banco de dados: " . $e->getMessage());
-}
-?>
